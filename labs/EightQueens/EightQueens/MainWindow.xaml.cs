6b029a91703b4d79306911935cbd6111c29991bf
Kaylee Lewis <klewis5@my.athens.edu>
2023-08-30 00:57:47 -0400

    Update MainWindow.xaml.cs

--- labs/EightQueens/EightQueens/MainWindow.xaml.cs
+++ labs/EightQueens/EightQueens/MainWindow.xaml.cs
@@ -1,17 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// Lab 02: Introduction to C#: A Classic Chess Problem
+// File:   EightQueens.xaml.cs
+// Author: Kaylee Lewis
+// Purpose:
+//   1. Continue to work with arrays in C#
+//   2. Study a classic problem from AI and algorithm theory
+
+using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace EightQueens
 {
@@ -20,12 +15,12 @@ namespace EightQueens
      /// </summary>
      public partial class MainWindow : Window
      {
-          static Random randomNumbers = new Random ( );
+          static Random randomNumbers = new Random();
 
-          static bool [ , ] board; // gameboard
+          static bool[,] board; // gameboard
 
           // accessibility values for each board position
-          static int [ , ] access =
+          static int[,] access =
           {
                { 22, 22, 22, 22, 22, 22, 22, 22 },
                { 22, 24, 24, 24, 24, 24, 24, 22 },
@@ -41,132 +36,133 @@ namespace EightQueens
 
           static int queens; // number of queens placed on the board
 
-          public static void UpdateAccess ( int row , int column )
+          public static void UpdateAccess(int row, int column)
           {
-               for ( int i = 0 ; i < 8 ; i++ )
+               for (int i = 0; i < 8; i++)
                {
                     // set elimination numbers to 99
                     // in the row occupied by the queen
-                    access [ row , i ] = maxAccess;
+                    access[row, i] = maxAccess;
 
                     // set elimination numbers to 99
                     // in the column occupied by the queen
-                    access [ i , column ] = maxAccess;
+                    access[i, column] = maxAccess;
                }
 
                // set elimination numbers to 99 in diagonals occupied by the queen
-               UpdateDiagonals ( row , column );
+               UpdateDiagonals(row, column);
           }
 
-          public static void UpdateDiagonals ( int rowValue , int colValue )
+          public static void UpdateDiagonals(int rowValue, int colValue)
           {
                int row = rowValue; // row position to be updated
                int column = colValue; // column position to be updated
 
                // upper left diagonal
-               for ( int diagonal = 0 ;
+               for (int diagonal = 0;
                     diagonal < 8 &&
-                    ValidMove ( --row , --column ) ;
-                    diagonal++ )
-                    access [ row , column ] = maxAccess;
+                    ValidMove(--row, --column);
+                    diagonal++)
+                    access[row, column] = maxAccess;
 
                row = rowValue;
                column = colValue;
 
                // upper right diagonal
-               for ( int diagonal = 0 ;
+               for (int diagonal = 0;
                     diagonal < 8 &&
-                    ValidMove ( --row , ++column ) ;
-                    diagonal++ )
-                    access [ row , column ] = maxAccess;
+                    ValidMove(--row, ++column);
+                    diagonal++)
+                    access[row, column] = maxAccess;
 
                row = rowValue;
                column = colValue;
 
                // lower left diagonal
-               for ( int diagonal = 0 ;
+               for (int diagonal = 0;
                     diagonal < 8 &&
-                    ValidMove ( ++row , --column ) ;
-                    diagonal++ )
-                    access [ row , column ] = maxAccess;
+                    ValidMove(++row, --column);
+                    diagonal++)
+                    access[row, column] = maxAccess;
 
                row = rowValue;
                column = colValue;
 
                // lower right diagonal
-               for ( int diagonal = 0 ;
+               for (int diagonal = 0;
                     diagonal < 8 &&
-                    ValidMove ( ++row , ++column ) ;
-                    diagonal++ )
-                    access [ row , column ] = maxAccess;
+                    ValidMove(++row, ++column);
+                    diagonal++)
+                    access[row, column] = maxAccess;
                // end method UpdateDiagonals
           }
 
-          public static bool ValidMove ( int row , int column )
+          public static bool ValidMove(int row, int column)
           {
-               return ( row >= 0 && row < 8 && column >= 0 && column < 8 );
+               return (row >= 0 && row < 8 && column >= 0 && column < 8);
           }
 
           // display the board
-          public static void PrintBoard ( )
+          public static void PrintBoard()
           {
-               Console.Write ( "  " );
+               Console.Write("  ");
 
                // display numbers for column
-               for ( int k = 0 ; k < 8 ; k++ )
-                    Console.Write ( " {0}" , k );
+               for (int k = 0; k < 8; k++)
+                    Console.Write(" {0}", k);
 
-               Console.WriteLine ( "\n" );
+               Console.WriteLine("\n");
 
-               for ( int row = 0 ; row < board.GetLength ( 0 ) ; row++ )
+               for (int row = 0; row < board.GetLength(0); row++)
                {
-                    Console.Write ( "{0} " , row );
+                    Console.Write("{0} ", row);
 
-                    for ( int column = 0 ; column < board.GetLength ( 1 ) ; column++ )
+                    for (int column = 0; column < board.GetLength(1); column++)
                     {
-                         if ( board [ row , column ] )
-                              Console.Write ( " Q" );
+                         if (board[row, column])
+                              Console.Write(" Q");
                          else
-                              Console.Write ( " ." );
+                              Console.Write(" .");
                     } // end for
 
-                    Console.WriteLine ( );
+                    Console.WriteLine();
                } // end for
 
-               Console.WriteLine ( "\n{0} queens placed on the board." , queens );
+               Console.WriteLine("\n{0} queens placed on the board.", queens);
           } // end method PrintBoard
-          public MainWindow ( )
+
+          public MainWindow()
           {
                int currentRow; // the row position on the chessboard
                int currentColumn; // the column position on the chessboard
 
-               board = new bool [ 8 , 8 ]; // all elements default to false
+               board = new bool [8, 8]; // all elements default to false
 
                // randomize initial first queen position
-               currentRow = randomNumbers.Next ( 8 );
-               currentColumn = randomNumbers.Next ( 8 );
+               currentRow = randomNumbers.Next(8);
+               currentColumn = randomNumbers.Next(8);
 
-               board [ currentRow , currentColumn ] = true;
+               board[currentRow, currentColumn] = true;
                ++queens;
 
-               UpdateAccess ( currentRow , currentColumn ); // update access
+               UpdateAccess(currentRow, currentColumn); // update access
 
                bool done = false;
                // continue until finished traversing
-               while ( !done )
+               while (!done)
                {
                     // the current lowest access number
                     int accessNumber = maxAccess;
 
                     // find square with the smallest elimination number
-                    for ( int row = 0 ; row < board.GetLength ( 0 ) ; row++ )
+                    for (int row = 0; row < board.GetLength(0); row++)
                     {
-                         for ( int col = 0 ; col < board.GetLength ( 1 ) ; col++ )
+                         for (int col = 0; col < board.GetLength(1); col++)
                          {
                               // obtain access number
-                              if ( access [ row , col ] < accessNumber )
+                              if (access[row, col] < accessNumber)
                               {
-                                   accessNumber = access [ row , col ];
+                                   accessNumber = access[row, col];
                                    currentRow = row;
                                    currentColumn = col;
                               }
@@ -174,18 +170,18 @@ namespace EightQueens
                     }
 
                     // traversing done
-                    if ( accessNumber == maxAccess )
+                    if (accessNumber == maxAccess)
                          done = true;
                     // mark the current location
                     else
                     {
-                         board [ currentRow , currentColumn ] = true;
-                         UpdateAccess ( currentRow , currentColumn );
+                         board[currentRow, currentColumn] = true;
+                         UpdateAccess(currentRow, currentColumn);
                          ++queens;
                     }
                }
 
-               PrintBoard ( );
+               PrintBoard();
           }
      }
 }
