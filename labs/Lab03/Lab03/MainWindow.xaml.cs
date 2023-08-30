f8b48518e31d9915eca0b758e3ef58b42cc945fa
Kaylee Lewis <klewis5@my.athens.edu>
2023-08-30 01:01:12 -0400

    Update MainWindow.xaml.cs

--- labs/Lab03/Lab03/MainWindow.xaml.cs
+++ labs/Lab03/Lab03/MainWindow.xaml.cs
@@ -1,17 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection.Emit;
-using System.Text;
-using System.Threading.Tasks;
+﻿// Lab 03: Demo 2-D Graphics in C# Using WPF
+// File:   Lab03.xaml.cs
+// Author: Kaylee Lewis
+// Purpose:
+//   1. Procedurally generate a tree using a fractal and some WPF code.
+
+using System;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Lab03
