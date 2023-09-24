fde73e4c7e75e3ac4344d214f299f924f1b6c368
Kaylee Lewis <klewis5@my.athens.edu>
2023-09-24 02:28:30 -0400

    LFS Museum

---
+++ labs/MuseumofEvil/Assets/Scripts/Teleporter Scripts/Room2Teleporter.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Make sure to add this, or you can't use SceneManager
+using UnityEngine.SceneManagement;
+
+
+public class Room2Teleporter : MonoBehaviour
+{
+
+     void OnTriggerEnter ( Collider other)
+     {
+          //other.name should equal the root of your Player object
+          //if (other.name == "AdvancedPlayer") {
+          //The scene number to load (in File->Build Settings)
+          Debug.Log ( "touched box" );
+          SceneManager.LoadScene ( "Room2" );
+          //}
+     }
+}
